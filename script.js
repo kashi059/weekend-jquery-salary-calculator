9d89194e1f22f732d2eb4bdf22ab488a30db8087
Joseph l Wipperfurth <35477479+kashi059@users.noreply.github.com>
2023-08-19 13:47:01 -0500

    Making solid progress though the delete button doesn't work yet

--- script.js
+++ script.js
@@ -1,6 +1,76 @@
-$(document).ready(onReady);
+$(document).ready(readyNow);
 //setup 
-function onReady() {
+function readyNow() {
+// $(submitButton).on(click){
+
+// }
+
+// on click were gonna run a function
+// save input fields 
+// assign them as either .text or .val to the table
+//${use these where you can} interpolation
+//
+
+//This is the on click section
+$ ('#submitButton').on('click', transferEntry)
+$('#removeEntryButton').on('click','.remove', removeEntry);
+}
+
+
+function transferEntry (event){
+//this prevents default settings
+    event.preventDefault();
+
+    console.log ('transferEntry function is running');
+    //target input element by ID
+    //get the value of the text input
+
+//DEFINING INPUTS
+let firstNameInput =  $('#firstName').val(); 
+let lastNameInput = $('#lastName').val();
+let idInput = $('#id').val();
+let titleInput = $('#title').val();
+let annualSalaryInput = $('#annualSalary').val();
+//its a number so do we need to do anything with this?^
+//same for id
+
+// setting up the things to go in
+    let addTableRow = `<tr id="insertionPoint">
+     <td>${firstNameInput}</td> 
+     <td>${lastNameInput}</td>
+     <td>${idInput}</td> 
+     <td>${titleInput}</td>
+     <td>${annualSalaryInput}</td> 
+     <td><button class ='remove'> <img id = "removeEntryButton" src = 'removeEntryButton.png' alt="Remove this section"> </button></td> 
+     </tr>` 
+        //insert the delete button
+
+//actually inserting the row of data
+     $('#mainTable').append(addTableRow);
+
+
+//clearing the data from the boxes
+$('#firsNameInput').val( '' );
+$('#lastNameInput').val( '' );
+$('#idInput').val( '' );
+$('#titleInput').val( '' );
+$('#annualSalaryInput').val( '' );
+
+//     let firsNameInput = $('#firstName').val();
+// console.log( "Welcome " + firsNameInput);
+// //empty out input (below)
+
 
 
 }
+
+function removeEntry(){
+    $(this).parents("tr").remove();
+}
+
+
+
+//TO DO 
+//make it clear the fields in the top
+//scale the delete button
+//make if till in each section instead of putting it all in the first box
